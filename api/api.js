f0d211fd248a951281a2dbfcf5aec09001f7fe63
Your Name <you@example.com>
2022-09-08 06:43:15 +0000

    ss

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 
 var http = require("http");
 export default function handler(req, resp) {
-	req.status(200).json({
+	resp.status(200).json({
 	  body: req.body,
 	  query: req.query,
 	  cookies: req.cookies,
